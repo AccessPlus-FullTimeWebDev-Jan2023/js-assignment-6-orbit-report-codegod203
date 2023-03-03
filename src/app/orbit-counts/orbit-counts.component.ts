6cb0635f79f4c4a8d85667c06d6c0247ffb72549
Vincent Grimes <grimesjamey@gmail.com>
2023-03-02 18:00:42 -0800

    changes

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -21,11 +21,12 @@ export class OrbitCountsComponent implements OnInit {
 	  for (let i = 0; i < this.satellites.length; i++) {
 		 if (this.satellites[i].type === type) {
 			count++;
+			console.log(count)
+
 		 }
 	  }
 	}
-	return count;
- }
+	
 
 
 }
